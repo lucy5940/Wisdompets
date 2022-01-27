63aabcbb2c179eb4ed32ff358a21b9f5614be7e3
lucy5940 <lpy0573@gmail.com>
2022-01-27 13:47:22 -0500

    commit1

--- wisdomets/adoptions/views.py
+++ wisdomets/adoptions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Pet
 
@@ -7,7 +8,17 @@ from .models import Pet
 def home(request):
     #return HttpResponse("<p> home view </p")
     pets = Pet.objects.all()
-    
+    return render(request,"home.html",{
+        "pets":pets,
+    })
+
 
 def pet_detail(request,pet_id):
-    return HttpResponse(f"<p> pet_detail viewwith {pet_id} </p>")
+    #return HttpResponse(f"<p> pet_detail viewwith {pet_id} </p>")
+    try:
+        pet = Pet.objects.get(id = pet_id)
+    except Pet.DoesNotExist:
+        raise Http404("pet not found")
+    return render(request,"pet_detail.html",{
+        "pet":pet,
+    })
